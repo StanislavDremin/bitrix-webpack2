7e1f8b890f3dfd5cc8dd5dcffaa4601851f0cc21
dres <st.dremin@ya.ru>
2017-10-20 11:35:05 +0300

    add config for webpack 3

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,66 +1,45 @@
 "use strict";
-const Encore = require('@symfony/webpack-encore');
+const Encore = require('webpackUtils/Encore.extend');
 import path from 'path';
-import EncodingPlugin from 'webpack-encoding-plugin';
 import FriendlyErrorsWebpackPlugin from 'friendly-errors-webpack-plugin';
+// раскоментить, если сайт работает в cp1251
+// import EncodingPlugin from 'webpack-encoding-plugin';
+
 import BComponent from './webpackUtils/BComponent';
 
 const app = (p = '') => {
-	let root = path.resolve(__dirname, 'online.1c.ru', 'public_html');
+	let root = path.resolve(__dirname, 'site.name', 'public_html');
 	return root + p;
 };
 
-Encore.BXComponent = new BComponent({root: path.resolve(__dirname, 'online.1c.ru', 'public_html')});
-
-Encore.BXComponentJs = (name, params = {}) => {
-	let entry = Encore.BXComponent.getEntry(
-		name,
-		Object.assign({}, {app: 'app.js', out: 'index', template: '.default', siteTemplate: false}, params)
-	);
-	return Encore.addEntry(entry.build, entry.app);
-};
-Encore.BXComponentStyle = (name, params = {}) => {
-	if(!params.hasOwnProperty('out')){
-		params.out = name.replace(':', '_') + '_' + params.template.replace('.', '');
-	}
-
-	let entry = Encore.BXComponent.getStyleEntry(
-		name,
-		Object.assign({}, {app: 'app.sass', template: '.default', siteTemplate: false}, params)
-	);
-	return Encore.addStyleEntry(entry.build, entry.app);
-};
+Encore.BXComponent = new BComponent({root: path.resolve(__dirname, 'site.name', 'public_html')});
 
 process.noDeprecation = true;
 Encore
-	.setOutputPath('online.1c.ru/public_html')
-	.setPublicPath(path.resolve(__dirname, 'online.1c.ru', 'public_html'))
+	.setOutputPath('site.name/public_html')
+	.setPublicPath(path.resolve(__dirname, 'site.name', 'public_html'))
 
+	// файлы выхода (out) надо указывать без расширения
 	.BXComponentJs('esd:grid', {out: 'script'})
 	.BXComponentStyle('esd:grid', {out: 'style'})
-	// .addEntry('local/components/esd/grid/buildTest.js', app('/local/components/esd/grid/app.js'))
-	// .addStyleEntry('local/components/esd/grid/data-style', app('/local/components/esd/grid/main.sass'))
 
 	.enableSassLoader()
 	.enableVueLoader()
 	.enableSourceMaps(false)
 
-	/*.addPlugin(new ExtractTextPlugin({
-		filename: 'local/components/esd/grid/style.css',
-		allChunks: true
-	}))*/
-	.addPlugin(new EncodingPlugin({encoding: "cp1251"}))
+	// если сайт работает в cp1251, то подключаем этот плагин,
+	// при этом все файлы для сборки (.js, .sass, .css и пр) должны быть в utf-8, плагин сам их конвертит при сборке
+	// .addPlugin(new EncodingPlugin({encoding: "cp1251"}))
 	.addPlugin(new FriendlyErrorsWebpackPlugin({clearConsole: true}))
 ;
 
-
-
 const config = Encore.getWebpackConfig();
+
 if(!config.resolve.hasOwnProperty('modules')){
 	config.resolve = Object.assign({}, config.resolve, {
 		modules: [
 			app('/local/src/js'),
-			'node_modules',
+			'node_modules', // указываем папку, откуда брать основные либы, иначе будут косяки
 		]
 	});
 
